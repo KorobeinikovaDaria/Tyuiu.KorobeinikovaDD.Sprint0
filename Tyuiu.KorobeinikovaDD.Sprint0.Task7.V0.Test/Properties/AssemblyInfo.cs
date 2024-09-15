f93912d964ed5d9df2a9ab7290cd12b05b7c09e1
Lenovo <Lenovo@LAPTOP-UGDF2950>
2024-09-15 22:19:20 +0300

    Task7

---
+++ Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c59b1ea-443a-4df5-adb7-6c44cb13472f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0.Lib;
+
+namespace Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #0 | Выполнила: Коробейникова Д.Д. | ПКТб-24-1";
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту            *");
+            Console.WriteLine("* Задание #7                                             *");
+            Console.WriteLine("* Вариант #0                                             *");
+            Console.WriteLine("* Выполнила: Коробейникова Дарья Денисовна | ПКТб-24-1   *");
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                               *");
+            Console.WriteLine("* Написать программу на С#, которая суммирует значения   *");
+            Console.WriteLine("* двух одинаковых массивов по длине.                     *");
+            Console.WriteLine("*                                                        *");
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                       *");
+            Console.WriteLine("**********************************************************");
+
+
+            int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Значение элементов массива №7");
+            for (int i = 0; i < arrayNums1.Length; i++)
+            {
+                Console.Write(arrayNums1[i] + ",");
+
+            }
+            Console.WriteLine();
+
+            int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Значение элементов массива №2");
+            for (int i = 0; i < arrayNums2.Length; i++)
+            {
+                Console.Write(arrayNums2[i] + ",");
+            }
+            Console.WriteLine();
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                             *");
+            Console.WriteLine("**********************************************************");
+            if (arrayNums1.Length == arrayNums2.Length)
+            {
+                int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+                Console.WriteLine("Сумма элментов массива ровна:");
+                for (int i = 0; i < resultArray.Length; i++)
+                {
+                    Console.Write(resultArray[i] + ",");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Горим!");
+            }
+                Console.ReadKey();
+            
+           
+        }
+    }
+}
+
