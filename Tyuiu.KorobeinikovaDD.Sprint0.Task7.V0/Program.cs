43bbcd863c8f6725881027a7161597eb6b1c3f7a
Lenovo <Lenovo@LAPTOP-UGDF2950>
2024-09-19 17:03:19 +0300

    Task7

--- Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0/Program.cs
+++ Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0/Program.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks;
 using Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0.Lib;
-
+ 
 namespace Tyuiu.KorobeinikovaDD.Sprint0.Task7.V0
-{ 
+{  
     internal class Program
     {
         static void Main(string[] args)
